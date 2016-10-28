83033d22765ee1256fdb1bf03a5fbd0733738992
greatontime <pathomphong1144@gmail.com>
2016-10-28 23:28:35 +0700

    start up module for odoo

---
+++ README.md
@@ -0,0 +1,1 @@
+# startup odoo module
---
+++ my_module/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name':"My Module",
+    'summary': "This is my first odoo module",
+    'description': "Amazing Odoo can do everything that I want",
+    'author': "Pathomphong",
+    'license': "AGPL-3",
+    'website': "http://www.printmego.com",
+    'category': "Uncategorized",
+    'version': '9.0.1.0.0',
+    'depends': ['base'],
+    'data': ['views/library_book.xml'],
+}
